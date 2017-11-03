bc12835392be237f3f0ca3850bc62992c04d6e1f
Oscar King <zcaboki@ucl.ac.uk>
2017-11-03 13:09:59 +0000

    tests

--- Team14Logbook/logbook/tests.py
+++ Team14Logbook/logbook/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class AddPostTest(LiveServerTestCase):
+	def setUp(self):
+		self.driver = webdriver.Chrome('/Users/sonik/Downloads/chromedriver')
+		super(AddPostTest, self).setUp()
+	def tearDown(self):
+		self.driver.quit()
+		super(AddPostTest, self).tearDown()
+	def test_addpost(self):
+		driver = self.driver
+		driver.get('http://127.0.0.1:8000/login/')
+		time.sleep(1)
+		driver.find_element_by_id("id_username").send_keys("admin")
+		time.sleep(1)
+		driver.find_element_by_id("id_password").send_keys("password123")
+		time.sleep(1)
+		driver.find_element_by_css_selector("input[type=submit]").click()
+		time.sleep(1)
+
+		driver.get('http://127.0.0.1:8000/create/')
+		time.sleep(1)
+
+		driver.find_element_by_id("id_title").send_keys("Papaver Rhoeas")
+		time.sleep(1)
+		driver.find_element_by_id("id_content").send_keys("This is a poppy.")
+		time.sleep(1)
+		driver.find_element_by_id("id_publish").send_keys("2017-11-03")
+		time.sleep(1)
+		driver.find_element_by_id("id_category").send_keys("Testing")
+		time.sleep(1)
+
+		driver.find_element_by_xpath('/html/body/div[2]/div/form/input[2]').send_keys(Keys.RETURN)
+
+		time.sleep(1)
